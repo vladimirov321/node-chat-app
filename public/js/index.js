bace71deaf283fe8a8d18fc8d5820886ad3338d3
Vladimir Vladimirov <v.vladimirov@deltastock.com>
2018-04-27 08:31:04 +0300

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server');
-    
-    socket.emit('createMessage', {
-        from: 'Vlado',
-        text: 'Hey'
-    });
 });
 
 socket.on('disconnect', function () {
